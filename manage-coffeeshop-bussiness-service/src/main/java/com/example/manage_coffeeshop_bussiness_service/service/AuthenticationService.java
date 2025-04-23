6fdb4105fd122ce65742e73e665710cc8db619df
dinhnguyetquynh <dquynhd202@gmail.com>
2025-04-23 23:07:58 +0700

    docker for redis

--- manage-coffeeshop-bussiness-service/src/main/java/com/example/manage_coffeeshop_bussiness_service/service/AuthenticationService.java
+++ manage-coffeeshop-bussiness-service/src/main/java/com/example/manage_coffeeshop_bussiness_service/service/AuthenticationService.java
@@ -22,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,7 +42,8 @@ import java.util.Date;
 public class AuthenticationService {
     private static final Logger log = LoggerFactory.getLogger(AuthenticationService.class);
     private final WebClient webClient;
-
+    @Autowired
+    private RedisService redisService;
     @NonFinal
     @Value("${jwt.signerKey}")
     protected String SIGNER_KEY ;
@@ -68,8 +70,11 @@ public class AuthenticationService {
             throw new RuntimeException("Invalid username or password");
         }
 
+
         String accessToken = generateToken(emp, 1); // 1 hour
-        String refreshToken = generateToken(emp, 7 * 24); // 7 days
+        String refreshToken = generateToken(emp, 7 * 24);// 7 days
+
+        redisService.saveRefreshToken(refreshToken,7 * 24 * 60 * 60);
 
         // Set refresh token in HttpOnly cookie
         Cookie cookie = new Cookie("refreshToken", refreshToken);
@@ -186,6 +191,32 @@ public class AuthenticationService {
 
 
 
+    }
+    private String extractTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("refreshToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = extractTokenFromCookie(request);
+        if (refreshToken != null && redisService.exists(refreshToken)) {
+            redisService.deleteRefreshToken(refreshToken);
+        }
+
+        // Xoá cookie phía client
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setMaxAge(0);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok("Đăng xuất thành công");
     }
 
 
